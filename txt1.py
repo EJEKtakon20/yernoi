5632fdcbb960e4ca077b7876f4d921b4ac8c00eb
unknown <takonejek@gmail.com>
2026-01-13 09:35:08 +0100

    New edit

---
+++ txt1.py
@@ -0,0 +1,13 @@
+name = "Takon"
+age = 20
+address = "Lagos"
+school = "Babcock University"
+workplace = "SystemSpecs"
+job_description = "DevOps Engineer"
+
+print("Name:", name)
+print("Age:", age)
+print("Address:", address)
+print("School:", school)
+print("Workplace:", workplace)
+print("Job Description:", job_description)
